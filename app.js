06b18a82093698f4f1bd41f87275e3a1c7a1fb5b
klvp <karenvalierpalmero@gmail.com>
2024-02-21 14:14:07 -0600

    Realice actividades del desafio

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+let title = document.querySelector('h1');
+title.innerHTML = 'Hora del Desafío';
 
+
+function activateButtonConsole(){
+    console.log('El botón fue cliclado');
+    
+    }
+function activateButtonPrompt(){
+    let city = prompt('Ingrese el nombre de una ciudad de Brasil porfavor');
+    alert(`Estuve en ${city} y me acordé de ti`);
+    
+}
+
+
+function activateButtonAlert(){
+    alert('Yo amo JS');
+
+}
+
+function addTwoNumbers(){
+    let number1 = Number(prompt('Ingrese un numero'));
+    let number2 = Number(prompt('Ingrese un numero'));
+   alert(number1 + number2);
+}
